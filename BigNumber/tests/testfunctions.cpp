877ba0ddc095c04aaf9256adb3655d5dfac350e1
FFMG <github@myoddweb.com>
2015-12-12 15:45:08 +0200

    Added more ToRadian/ToDegree
    Negative cases.

--- BigNumber/tests/testfunctions.cpp
+++ BigNumber/tests/testfunctions.cpp
@@ -1330,6 +1330,20 @@ TEST(FunctionBigNumber, ThirtyDegreesToRadian)
   }
 }
 
+TEST(FunctionBigNumber, NegativeThirtyDegreesToRadian)
+{
+  {
+    MyOddWeb::BigNumber rad = MyOddWeb::BigNumber(-30).ToRadian(10);
+    std::string z = rad.ToString();
+    ASSERT_EQ("-0.5235987756", z);
+  }
+  {
+    MyOddWeb::BigNumber rad = MyOddWeb::BigNumber(-30).ToRadian(20);
+    std::string z = rad.ToString();
+    ASSERT_EQ("-0.52359877559829887308", z);
+  }
+}
+
 TEST(FunctionBigNumber, ThirtyDegreesRadianToDegree)
 {
   {
@@ -1344,9 +1358,31 @@ TEST(FunctionBigNumber, ThirtyDegreesRadianToDegree)
   }
 }
 
+TEST(FunctionBigNumber, NegativeThirtyDegreesRadianToDegree)
+{
+  {
+    MyOddWeb::BigNumber rad = MyOddWeb::BigNumber("-0.5235987756").ToDegree(10);
+    std::string z = rad.ToString();
+    ASSERT_EQ("-30.0000000001", z);
+  }
+  {
+    MyOddWeb::BigNumber rad = MyOddWeb::BigNumber("-0.52359877559829887308").ToDegree(20);
+    std::string z = rad.ToString();
+    ASSERT_EQ("-30.00000000000000000017", z);
+  }
+}
+
 TEST(FunctionBigNumber, PiToDegree)
 {
   MyOddWeb::BigNumber rad = MyOddWeb::BigNumber(MyOddWeb::BigNumber::pi() ).ToDegree(10);
   std::string z = rad.ToString();
   ASSERT_EQ("180", z);
+}
+
+TEST(FunctionBigNumber, NegativePiToDegree)
+{
+  MyOddWeb::BigNumber rad = MyOddWeb::BigNumber(MyOddWeb::BigNumber::pi()).ToDegree(10);
+  rad.Mul(-1);
+  std::string z = rad.ToString();
+  ASSERT_EQ("-180", z);
 }
