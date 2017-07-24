ffe3f7887cc3d943e4947a9b8c6a28522f105db2
Florent Guelfucci <florent.guelfucci@derivco.co.za>
2017-07-24 13:20:54 +0200

    Created blank, broken, gcc project

---
+++ gcc/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+#include "BigNumber.h"
+
+int main()
+{
+  MyOddWeb::BigNumber x = "1000";
+  MyOddWeb::BigNumber y = "1000";
+  std::cout << x.Mul( y ).ToString() << std::endl;
+  return 0;
+}
