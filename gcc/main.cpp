a0b17cbdb082e461c80f3024f7d48d1ac055fe58
Florent Guelfucci <florent.guelfucci@derivco.co.za>
2017-07-25 17:05:51 +0200

    Some more div tries

--- gcc/main.cpp
+++ gcc/main.cpp
@@ -22,23 +22,42 @@ void Div1( const MyOddWeb::BigNumber& x, const MyOddWeb::BigNumber& y)
 
 void Div2( const MyOddWeb::BigNumber& x, const MyOddWeb::BigNumber& y)
 {
-  MyOddWeb::BigNumber min = 0;
+  // save the min max
+  MyOddWeb::BigNumber min = 1;
   MyOddWeb::BigNumber max = x;
 
   for(;;)
   {
-    MyOddWeb::BigNumber z = max;
-    z.Sub( min ).Add(min).Div(2);
+    MyOddWeb::BigNumber z = MyOddWeb::BigNumber(max).Add( min ).Div(2);
 
-    switch( z.Mul(y).Compare( x ) )
+    auto dm1 = min.ToDouble();
+    auto dm2 = max.ToDouble();
+    auto dm3 = z.ToDouble();
+
+    auto a =  MyOddWeb::BigNumber(z).Mul(y);
+    auto dm4 = a.ToDouble();
+
+    auto b =  MyOddWeb::BigNumber(x).Sub(a);
+
+    switch( a.Compare( x ) )
     {
     case 0:
-      break;
+      // it is exactly the same
+      // there is no remainder
+      return;
 
     case 1:
+      // ((max - min)/2)*y than greater
+      // so we know that the number has to be smaller than 'z'
+      // our new max is 'z'
+      max = z;
       break;
 
     case -1:
+      // ((max - min)/2)*y than smaller
+      // so we know that the number has to be greater than 'z'
+      // our new min is 'z'
+      min = z;
       break;
     }
   }
@@ -50,12 +69,12 @@ int main(int argc, char** argv)
   MyOddWeb::BigNumber x = "1234567890123456789012345678901234546789012345678901234567890";
   MyOddWeb::BigNumber y = "1230";
 
-  TIMER_START
-  Div1( x, y );
-  TIMER_END
+//  TIMER_START
+//  Div1( x, y );
+//  TIMER_END
 
   TIMER_START
-  Div1( x, y );
+  Div2( 17, 3 );
   TIMER_END
 
   return 0;
